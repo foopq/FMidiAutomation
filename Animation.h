0fc8b9c713b432d607563574aac4185e7ec1a9fd
Chris Mennie <cmennie@rogers.com>
2011-07-31 11:24:13 -0400

    Updated licensing

--- Animation.h
+++ Animation.h
@@ -1,3 +1,10 @@
+/*
+FMidiAutomation -- A midi automation editor for jack / Linux
+Written by Chris Mennie (chris at chrismennie.ca or cmennie at rogers.com)
+Copyright (C) 2011 Chris A. Mennie                              
+
+License: Released under the GPL version 3 license. See the included LICENSE.
+*/
 
 
 #ifndef __ANIMATION_H
--- FMidiAutomationData.h
+++ FMidiAutomationData.h
@@ -1,3 +1,12 @@
+/*
+FMidiAutomation -- A midi automation editor for jack / Linux
+Written by Chris Mennie (chris at chrismennie.ca or cmennie at rogers.com)
+Copyright (C) 2011 Chris A. Mennie                              
+
+License: Released under the GPL version 3 license. See the included LICENSE.
+*/
+
+
 #ifndef __FMIDIAUTOMATIONDATA_H
 #define __FMIDIAUTOMATIONDATA_H
 
